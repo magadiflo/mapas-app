5a843c51af745c7b56ffec3b0a0ec770743f788d
Martín <magadiflo@gmail.com>
2022-01-19 12:42:46 -0500

    Página zoom range - diseño y tarea

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as mapboxgl from 'mapbox-gl';
+
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `.mapa-container {
+      width: 100%;
+      height: 100%;
+    }
+    
+    .row {
+      background-color: white;
+      position: fixed;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      border-radius: 5px;
+      z-index: 999;
+    }`
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +27,12 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-78.52339270267693, -9.131787495382964],
+      zoom: 16,
+    });
   }
 
 }
